ea089ae8721a54243d75a9c1aa9a0d00d83364cc
CRAZYHACKS001 <redmoonplayz309@gmail.com>
2024-08-06 11:00:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def write_config(config):
 
 config = load_config()
 bot = telebot.TeleBot(config['bot_token'])
-ADMIN_IDS = set(config['admin_ids'])
+ADMIN_IDS = set(config['6868412487'])
 USER_FILE = config['user_file']
 LOG_FILE = config['log_file']
 COOLDOWN_TIME = config['cooldown_time']
